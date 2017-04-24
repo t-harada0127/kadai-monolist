a53cebb49eac860d303c53b9e98c164bb6c789cc
Toshiaki Harada <t.harada@nsl.cs.waseda.ac.jp>
2017-04-24 02:16:52 +0000

    have

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -13,6 +13,8 @@ class OwnershipsController < ApplicationController
     if params[:type] == 'Want'
       current_user.want(@item)
       flash[:success] = '商品をWantしました。'
+    elsif params[:type] == 'Have'
+      current_user.have(@item) 
     end
 
     redirect_back(fallback_location: root_path)
@@ -24,6 +26,10 @@ class OwnershipsController < ApplicationController
     if params[:type] == 'Want'
       current_user.unwant(@item)
       flash[:success] = '商品のWantを解除しました。'
+    elsif params[:type] == 'Have'
+      current_user.unhave(@item)
+      flash[:success] = '商品のHaveを解除しました。'   
+
     end
 
     redirect_back(fallback_location: root_path)
